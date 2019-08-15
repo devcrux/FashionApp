6d9d856aa168e0e8da9b05f9fd22bbbc452785e5
Oludayo Alli <sunnymexy@gmail.com>
2019-08-15 08:19:41 +0100

    Add project files.

---
+++ FashionApp/FashionApp/Views/ShopPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FashionApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShopPage : ContentPage
+    {
+        public ShopPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        private Timer timer;
+        public List<Banner> Banners { get => GetBanners(); }
+        public List<Product> CollectionsList { get => GetCollections(); }
+        public List<Product> TrendsList { get => GetTrends(); }
+
+        private List<Banner> GetBanners()
+        {
+            var bannerList = new List<Banner>();
+            bannerList.Add(new Banner { Heading = "SUMMER COLLECTION", Message = "40% Discount", Caption = "BEST DISCOUNT THIS SEASON", Image = "classic.png" });
+            bannerList.Add(new Banner { Heading = "WOMEN'S CLOTHINGS", Message = "UP TO 50% OFF", Caption = "GET 50% OFF ON EVERY ITEM", Image = "womenCol.png" });
+            bannerList.Add(new Banner { Heading = "ELEGANT COLLECTION", Message = "20% Discount", Caption = "UNIQUE COMBINATIONS OF ITEMS", Image = "elegantCol.png" });
+            return bannerList;
+        }
+
+        private List<Product> GetCollections()
+        {
+            var trendList = new List<Product>();
+            trendList.Add(new Product { Image = "floral.png", Name = "Floral Bag + Hat", Price = "$123.50" });
+            trendList.Add(new Product { Image = "satchel.png", Name = "Satchel Bag", Price = "$49.99" });
+            trendList.Add(new Product { Image = "leatherBag.png", Name = "Leather Bag", Price = "$40.99" });
+            return trendList;
+        }
+
+        private List<Product> GetTrends()
+        {
+            var colList = new List<Product>();
+            colList.Add(new Product { Image = "heeledShoe.png", Name = "Beige Heeled Shoe", Price = "$109.99" });
+            colList.Add(new Product { Image = "dressShoe.png", Name = "Shoe + Addons", Price = "$225.99" });
+            return colList;
+        }
+
+        protected override void OnAppearing()
+        {
+            timer = new Timer(TimeSpan.FromSeconds(5).TotalMilliseconds) { AutoReset = true, Enabled = true };
+            timer.Elapsed += Timer_Elapsed;
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            timer?.Dispose();
+            base.OnDisappearing();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+
+                if (cvBanners.Position == 2)
+                {
+                    cvBanners.Position = 0;
+                    return;
+                }
+
+                cvBanners.Position += 1;
+            });
+        }
+    }
+
+    public class Banner
+    {
+        public string Heading { get; set; }
+        public string Message { get; set; }
+        public string Caption { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+    }
+}
